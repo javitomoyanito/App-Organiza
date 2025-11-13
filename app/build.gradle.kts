38466123ee0bedb8ab7167edb6d09e738f237edf
javier <bravomoyanojavier@gmail.com>
2025-11-12 22:41:13 -0300

    ApiGoogle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
+
 
 
 }
